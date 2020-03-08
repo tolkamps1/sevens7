be976766e3ec0c9727687f35619646cdfe7aa7a2
Shaelyn Tolkamp <tolkamps1@uvic.ca>
2020-03-08 10:05:57 -0700

    clean up

--- app.test.py
+++ app.test.py
@@ -4,14 +4,7 @@ import requests
 from selenium import webdriver
 
 driver = webdriver.Firefox()
-driver.get("http://localhost:8080/tetris/games/static")
-assert "Tetris Games" in driver.title
-elem = driver.find_elements_by_css_selector("tr.uk-card")
-
-response = requests.get('http://localhost:8080/tetris/games')
-body = json.loads(response.content.decode('utf-8'))
-non_zero_games = [game for game in body if game['score'] != 0]
-
-assert len(non_zero_games) == len(elem)-1
+driver.get("http://localhost:8080/")
+assert "Sevens7 Card Game" in driver.title
 
 driver.close()
